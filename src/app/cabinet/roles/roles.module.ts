6568fc8c6c59a8d5f6c2fd224e897b3b28ddcac7
tkuku <TarasK@natr-temps.com>
2023-01-11 15:13:41 +0200

    (feature) : Refactor modules of cabinet

--- src/app/cabinet/roles/roles.module.ts
+++ src/app/cabinet/roles/roles.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
-import { CabinetComponent } from './cabinet.component';
-import { CabinetRoutingModule } from './cabinet-routing.module';
-import { HeaderComponent } from './template/header/header.component';
-import { SidebarComponent } from './template/sidebar/sidebar.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PaginationService } from '../services/cabinet/shared/pagination/pagination.service';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PaginationService } from '../../services/cabinet/shared/pagination/pagination.service';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
@@ -18,29 +12,28 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
-import { WarningConfirmationComponent } from './shared/warning-confirmation/warning-confirmation.component';
-import { AuthInterceptor } from './auth-interceptor';
+import { AuthInterceptor } from '../auth-interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
-import { PermissionsModule } from './permissions/permissions.module';
-import { StatusPipesModule } from '../pipes/cabinet/status/status.pipe.module';
-import { RolesModule } from './roles/roles.module';
-import { UsersModule } from './users/users.module';
+import { StatusPipesModule } from '../../pipes/cabinet/status/status.pipe.module';
+import { RouterModule } from '@angular/router';
+import { RolesComponent } from './roles.component';
+import { RoleDetailComponent } from './role-detail/role-detail.component';
+import { RoleCreateComponent } from './role-create/role-create.component';
+import { RoleEditComponent } from './role-edit/role-edit.component';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    HomeComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    CabinetComponent,
-    WarningConfirmationComponent
+    RolesComponent,
+    RoleDetailComponent,
+    RoleCreateComponent,
+    RoleEditComponent,
   ],
   imports: [
     CommonModule,
-    CabinetRoutingModule,
     ReactiveFormsModule,
+    RouterModule,
     FlexLayoutModule,
     MatButtonModule,
     MatListModule,
@@ -52,12 +45,9 @@ import { UsersModule } from './users/users.module';
     MatCardModule,
     MatDialogModule,
     TranslateModule,
-    PermissionsModule,
-    RolesModule,
-    UsersModule,
     StatusPipesModule
   ],
-  exports: [CabinetComponent, TranslateModule],
+  exports: [RolesComponent, TranslateModule],
   providers: [
     PaginationService,
     {
@@ -68,4 +58,4 @@ import { UsersModule } from './users/users.module';
   ],
   bootstrap: []
 })
-export class CabinetModule { }
+export class RolesModule { }
